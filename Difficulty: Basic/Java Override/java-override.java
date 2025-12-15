075cac0368702702cea55de0ebb28e361a9d3781
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-15 09:41:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Override/java-override.java
@@ -0,0 +1,12 @@
+class Child extends Parent {
+    
+    @Override
+    void show(int p) {
+        System.out.print(p + " ");
+    }
+
+    @Override
+    void print(int q) {
+        System.out.println(q * q);
+    }
+}
