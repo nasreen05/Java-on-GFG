59fd4d1ba3ec3084257783a30f25aa9e4cf90705
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-18 08:44:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM/lcm.java
+++ Difficulty: Basic/LCM/lcm.java
@@ -1,16 +1,17 @@
 class Solution {
-    
-    // Function to calculate GCD using Euclidean algorithm
-    public static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-    
     public static int LCM(int a, int b) {
-        return (a * b) / gcd(a, b);
+        int x = a, y = b;
+
+        // Find GCD using Euclidean algorithm
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+
+        // LCM formula
+        return (a / gcd) * b;
     }
 }
