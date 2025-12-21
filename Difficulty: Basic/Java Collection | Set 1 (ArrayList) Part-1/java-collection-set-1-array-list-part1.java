b686271f29f2344ca9674c1e7a7f11d69d9e5323
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-21 23:35:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Java Collection | Set 1 (ArrayList) Part-1/java-collection-set-1-array-list-part1.java
+++ Difficulty: Basic/Java Collection | Set 1 (ArrayList) Part-1/java-collection-set-1-array-list-part1.java
@@ -4,27 +4,26 @@ class Solution {
     ArrayList<Integer> solve(int N, ArrayList<Integer> A, int Q,
                              ArrayList<Integer> Query) {
 
-        ArrayList<Integer> result = new ArrayList<>();
-
-        // Query type 1: Insert
+        // Query type 1: Insert element
         if (Q == 1) {
             int p = Query.get(0);
             int r = Query.get(1);
-            A.add(p, r);      // insert r at index p
+            A.add(p, r);          // insert r at index p
             return A;
         }
 
-        // Query type 2: Last index of element
+        // Query type 2: Find last index of element
         else {
             int p = Query.get(0);
             int index = -1;
 
             for (int i = 0; i < A.size(); i++) {
                 if (A.get(i) == p) {
-                    index = i;
+                    index = i;    // keep updating for last occurrence
                 }
             }
 
+            ArrayList<Integer> result = new ArrayList<>();
             result.add(index);
             return result;
         }
