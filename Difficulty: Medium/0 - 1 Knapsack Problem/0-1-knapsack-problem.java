3a55d0980d3804e136625b07b1480f88c6fcedee
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-27 12:12:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int knapsack(int W, int val[], int wt[]) {
+        int n = val.length;
+        
+        int[][] dp = new int[n + 1][W + 1];
+        
+        // Build DP table
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= W; w++) {
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                        dp[i - 1][w],
+                        val[i - 1] + dp[i - 1][w - wt[i - 1]]
+                    );
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+        
+        return dp[n][W];
+    }
+}
+
